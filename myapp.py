e0cf87b27f4a54c96d806d18d5d6c16a4fad60da
loop775 <loopstyle108@gmail.com>
2025-06-16 15:25:29 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -121,4 +121,5 @@ def home():
     return render_template_string(HTML_TEMPLATE, winners=ipl_winners)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
